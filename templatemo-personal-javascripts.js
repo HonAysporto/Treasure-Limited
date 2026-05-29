9306d5eb04e440f5a4d4341b3458af46f50d8e98
Awosika Ayomide <ayomideoluwafemi2019@gmail.com>
2026-05-29 03:39:21 +0100

    adjusted

--- templatemo-personal-javascripts.js
+++ templatemo-personal-javascripts.js
@@ -176,3 +176,25 @@ https://templatemo.com/tm-593-personal-shape
                 document.body.style.overflow = 'auto';
             }
         });
+
+
+        
+    const carousels = document.querySelectorAll('.carousel');
+
+    carousels.forEach(carousel => {
+
+        const images = carousel.querySelectorAll('img');
+
+        let index = 0;
+
+        setInterval(() => {
+
+            images[index].classList.remove('active');
+
+            index = (index + 1) % images.length;
+
+            images[index].classList.add('active');
+
+        }, 5000);
+
+    });
